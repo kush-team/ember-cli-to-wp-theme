53515fa74ceb2f57fc3d8960a8af81fbab82ebec
Maximiliano Ivan Biondo <mbiondo@goblab.org>
2015-05-07 20:04:20 -0300

    blueprint fixed

---
+++ blueprints/ember-cli-to-wp-theme/files/app/adapters/application.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import WPAdapter from 'ember-cli-to-wp-theme/adapters/application';
+
+export default WPAdapter.extend({
+	host: '',
+});
--- blueprints/ember-cli-to-wp-theme/index.js
+++ blueprints/ember-cli-to-wp-theme/index.js
@@ -1,19 +1,4 @@
 module.exports = {
   description: 'Generates an ember-data adapter.',
-
-  availableOptions: [
-    { name: 'base-class', type: String }
-  ],
-
-  locals: function(options) {
-    var baseClass       = 'WPAdapter';
-    var importStatement = 'import WPAdapter from \'ember-cli-to-wp-theme/adapters/application\';';
-    options.baseClass = 'application';
- 
-
-    return {
-      importStatement: importStatement,
-      baseClass: baseClass
-    };
-  }
+  normalizeEntityName: function() {}
 };
