42c38f4e3cff8de5057c907d4112ce1700cb73fc
MBiondo <mbiondo@goblab.org>
2015-05-07 00:16:57 -0300

    Blueprint

--- blueprints/ember-cli-to-wp-theme/index.js
+++ blueprints/ember-cli-to-wp-theme/index.js
@@ -6,24 +6,10 @@ module.exports = {
   ],
 
   locals: function(options) {
-    var adapterName     = options.entity.name;
     var baseClass       = 'WPAdapter';
     var importStatement = 'import WPAdapter from \'ember-cli-to-wp-theme/adapters/application\';';
-
-    if (!options.baseClass && adapterName !== 'application') {
-      options.baseClass = 'application';
-    }
-
-    if (options.baseClass === adapterName) {
-      throw new SilentError('Adapters cannot extend from themself. To resolve this, remove the `--base-class` option or change to a different base-class.')
-    }
-
-    if (options.baseClass) {
-      baseClass = stringUtil.classify(options.baseClass.replace('\/', '-'));
-      baseClass = baseClass + 'Adapter';
-
-      importStatement = 'import ' + baseClass + ' from \'./' + options.baseClass + '\';'
-    }
+    options.baseClass = 'application';
+ 
 
     return {
       importStatement: importStatement,
